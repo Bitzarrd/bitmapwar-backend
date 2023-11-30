50ba9375e61f5ed6aa9ae514846f1a346684321f
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-30 14:51:32 +0800

    init

--- bitmap_client/src/main.js
+++ bitmap_client/src/main.js
@@ -7,11 +7,27 @@ import {store} from "./store"
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import VueNativeSock from "vue-native-websocket-vue3";
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
 app.use(store)
+
+
+// const mutations = {
+//     SOCKET_ONOPEN,
+//     SOCKET_ONCLOSE,
+//     SOCKET_ONERROR,
+//     SOCKET_ONMESSAGE,
+//     SOCKET_RECONNECT,
+//     SOCKET_RECONNECT_ERROR
+// }
+
+app.use(VueNativeSock, 'ws://localhost:6000', {
+    store: store,
+    // mutations: mutations
+})
 
 app.mount('#app')
--- bitmap_client/src/store/index.js
+++ bitmap_client/src/store/index.js
@@ -3,12 +3,39 @@ import {createStore} from "vuex";
 export const store = createStore({
     state() {
         return {
-            count: 0
+            socket: {
+                isConnected: false,
+                message: '',
+                reconnectError: false,
+            }
         }
     },
     mutations: {
-        increment(state) {
-            state.count++
+        SOCKET_ONOPEN(state, event) {
+            // Vue.prototype.$socket = event.currentTarget
+            state.socket.isConnected = true
+        },
+        SOCKET_ONCLOSE(state, event) {
+            state.socket.isConnected = false
+        },
+        SOCKET_ONERROR(state, event) {
+            console.error(state, event)
+        },
+        // default handler called for all methods
+        SOCKET_ONMESSAGE(state, message) {
+            state.socket.message = message
+        },
+        // mutations for reconnect methods
+        SOCKET_RECONNECT(state, count) {
+            console.info(state, count)
+        },
+        SOCKET_RECONNECT_ERROR(state) {
+            state.socket.reconnectError = true;
+        },
+    },
+    actions: {
+        sendMessage: (context, message) => {
+            // Vue.prototype.$socket.sendObj(message)
         }
     }
 })
