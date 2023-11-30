ab122d4d6f0c817e8387402820a069872b96765d
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-30 12:05:20 +0800

    init

--- bitmap_sdk/index.js
+++ bitmap_sdk/index.js
@@ -13,3 +13,105 @@ export function generate2DArray(width, height) {
 
     return array;
 }
+
+export function isCoordinateInArray(array, x, y) {
+    if (x < 0 || y < 0 || x >= array.length || y >= array[0].length) {
+        return false;
+    }
+
+    return true;
+}
+
+export function getCircleCoordinates(radius) {
+    const coordinates = [];
+    let x = 0;
+    let y = 0;
+
+    for (let i = 1; i <= radius; i++) {
+        // 上边
+        for (let j = -i; j <= i; j++) {
+            coordinates.push([x - i, y + j]);
+        }
+
+        // 右边
+        for (let j = -i + 1; j <= i; j++) {
+            coordinates.push([x + j, y + i]);
+        }
+
+        // 下边
+        for (let j = -i + 1; j <= i; j++) {
+            coordinates.push([x + i, y - j]);
+        }
+
+        // 左边
+        for (let j = -i + 1; j < i; j++) {
+            coordinates.push([x - j, y - i]);
+        }
+    }
+
+    return coordinates;
+}
+
+export function drawCell(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+    ctx.strokeStyle = 'rgb(128, 128, 128)';
+    ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+export function renderGrid() {
+    for (let i = 0; i < gridWidth; i++) {
+        for (let j = 0; j < gridHeight; j++) {
+            const cellColor = grid[i][j] === 0 ? 'gery' : grid[i][j];
+            drawCell(i, j, cellColor);
+        }
+    }
+}
+
+export function updateCell(x, y, color) {
+    grid[x][y] = color;
+    drawCell(x, y, color);
+}
+
+export function run(player) {
+    if (player.i <= player.circle.length) {
+        let cell = player.circle[player.i];
+        if (!cell) {
+            return;
+        }
+        let x = player.x + cell[0];
+        let y = player.y + cell[1];
+        while (!isCoordinateInArray(grid, x, y)) {
+            player.i++;
+            let cell = player.circle[player.i];
+            x = player.x + cell[0];
+            y = player.y + cell[1];
+        }
+        updateCell(x, y, player.color);
+        player.i++;
+    }
+}
+
+export function drawHorizontalLine(y) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+}
+
+export function drawVerticalLine(x) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+}
+
+export function drawGrid() {
+    for (let i = 0; i < gridHeight; i++) {
+        drawHorizontalLine(i * cellSize);
+    }
+
+    for (let j = 0; j < gridWidth; j++) {
+        drawVerticalLine(j * cellSize);
+    }
+}
