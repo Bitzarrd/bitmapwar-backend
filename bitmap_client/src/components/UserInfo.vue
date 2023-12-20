355875c2baac83d64f3d41d5cf59249d5beb10c8
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-12-20 16:46:28 +0800

    ShareNetwork Twitter

--- bitmap_client/src/components/UserInfo.vue
+++ bitmap_client/src/components/UserInfo.vue
@@ -2,11 +2,11 @@
 import {mapMutations, mapState} from "vuex";
 import {shortend} from "@/utils";
 import {formatEther} from "ethers";
-import {CirclePlus, Coin, View} from "@element-plus/icons-vue";
+import {CirclePlus, Coin, Share, View} from "@element-plus/icons-vue";
 
 export default {
   name: "UserInfo",
-  components: {Coin, View, CirclePlus},
+  components: {Share, Coin, View, CirclePlus},
   computed: {
     ...mapState(['wallet_address', 'user', 'selected_map'])
   },
@@ -63,7 +63,7 @@ export default {
               placement="top"
           >
           <el-input :value="formatEther(user.profit)" disabled
-                    style="float: left;display: inline;width: 100px;margin-right: 10px"/>
+                    style="float: left;display: inline;width: 110px;margin-right: 10px"/>
           </el-tooltip>
           <el-button @click="onClickProfits" style="float: right;display: inline">
             <el-icon color="white" class="no-inherit">
@@ -73,7 +73,7 @@ export default {
         </el-form-item>
         <el-form-item label="Bitmaps:">
           <el-input :value="selected_map" disabled
-                    style="float: left;display: inline;width: 100px;margin-right: 10px"/>
+                    style="float: left;display: inline;width: 110px;margin-right: 10px"/>
           <el-button @click="onClickBitmapList" style="float: right;display: inline">
             <el-icon color="white" class="no-inherit">
               <View/>
@@ -82,12 +82,32 @@ export default {
         </el-form-item>
         <el-form-item label="Soldier:">
           <el-input :value="user.virus" disabled
-                    style="float: left;display: inline;width: 100px;margin-right: 10px"/>
+                    style="float: left;display: inline;width: 110px;margin-right: 10px"/>
           <el-button @click="onClickPurchase" style="float: right;display: inline">
             <el-icon color="white" class="no-inherit">
               <CirclePlus/>
             </el-icon>
           </el-button>
+        </el-form-item>
+        <el-form-item label="Freeclaim">
+          <el-input disabled placeholder="500 Soldier" style="float: left;display: inline;width: 110px;margin-right: 10px"/>
+
+          <ShareNetwork
+              network="Twitter"
+              url="https://news.vuejs.org/issues/180"
+              title="Say hi to Vite! A brand new, extremely fast development setup for Vue."
+              description="This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You."
+              quote="The hot reload is so fast it\'s near instant. - Evan You"
+              hashtags="vuejs,vite"
+          >
+            <el-button  style="float: right;display: inline">
+              <el-icon color="white" class="no-inherit">
+                <Share/>
+              </el-icon>
+            </el-button>
+          </ShareNetwork>
+
+
         </el-form-item>
       </el-form>
     </el-card>
