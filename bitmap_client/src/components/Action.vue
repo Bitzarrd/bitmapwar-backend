60faa14751278bf17f097989a4057d510f3507ba
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-12-27 12:35:42 +0800

    1

--- bitmap_client/src/components/Action.vue
+++ bitmap_client/src/components/Action.vue
@@ -74,77 +74,86 @@ export default {
     onClickSubmit() {
       this.setActionDialogVisible(true);
     },
+    onClickColor(color){
+      this.selected_color = color;
+    }
   }
 }
 </script>
 
 <template>
   <div class="combg">
-    <div class="titc com_flex3"><img src="images/ico4.png"/><em>Action</em></div>
-    <ul class="act_form">
+    <div class="titc com_flex3">
+      <img src="images/ico4.png"/>
+      <em>Action</em>
+    </div>
+    <ul class="act_form" v-if="wallet_address">
       <li><em>Function:</em>
-        <div class="ck_c com_flex1"><span class="cur"><i class="ibg ibg1"></i></span><span><i
-            class="ibg ibg2"></i></span><span><i class="ibg ibg3"></i></span><span><i
-            class="ibg ibg4"></i></span></div>
+        <div class="ck_c com_flex1">
+          <span class="cur"><i class="ibg ibg1" @click="onClickColor('red')"></i></span>
+          <span><i class="ibg ibg2" @click="onClickColor('blue')"></i></span>
+          <span><i class="ibg ibg3" @click="onClickColor('red')"></i></span>
+          <span><i class="ibg ibg4" @click="onClickColor('red')"></i></span>
+        </div>
       </li>
       <li><em>Bitmap:</em>
-        <p>818888</p></li>
+        <p>{{ selected_map }}</p></li>
       <li><em>Owner:</em>
-        <p>0x86d...c8319</p></li>
-      <li><em>Soldier:</em><input class="a_intxt" type="text" value="100"/></li>
+        <p>{{ shortend(wallet_address) }}</p></li>
+      <li><em>Soldier:</em><input class="a_intxt" type="text" v-model="virus"/></li>
     </ul>
-    <div class="act_bot"><input type="button" value="Submit" class="submit"/></div>
+    <div class="act_bot"><input type="button" value="Submit" class="submit" @click="onClickSubmit"/></div>
   </div>
-<!--  <div class="mycard">-->
-<!--    <el-card class="box-card">-->
-<!--      <template #header>-->
-<!--        <div class="card-header">-->
-<!--          <el-icon color="white" class="no-inherit">-->
-<!--            <Edit/>-->
-<!--          </el-icon>-->
-<!--          <span> Action</span>-->
-<!--        </div>-->
-<!--      </template>-->
-<!--      <el-form label-width="80px" v-if="wallet_address">-->
-<!--        <el-form-item label="Function:">-->
-<!--          <el-select v-model="color" class="m-2" placeholder="Select" :disabled="lock">-->
-<!--            <el-option-->
-<!--                v-for="item in options"-->
-<!--                :key="item.value"-->
-<!--                :label="item.label"-->
-<!--                :value="item.value"-->
-<!--            >-->
-<!--                        <span :style="{ color: item.value }">-->
-<!--                          {{ item.label }}-->
-<!--                        </span>-->
-<!--            </el-option>-->
-<!--          </el-select>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Bitmap:">-->
-<!--          <el-input :value="selected_map" disabled/>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Owner:">-->
-<!--          <el-tooltip-->
-<!--              class="box-item"-->
-<!--              effect="dark"-->
-<!--              :content="wallet_address"-->
-<!--              placement="top"-->
-<!--          >-->
-<!--            <el-input :value="shortend(wallet_address)" disabled/>-->
-<!--          </el-tooltip>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Soldier:">-->
-<!--          <el-input-number :controls="false" :min="1" v-model="virus"/>-->
-<!--        </el-form-item>-->
-<!--      </el-form>-->
-<!--      <template #footer>-->
-<!--        <div style="display: flex; justify-content: right;" v-if="wallet_address">-->
-<!--          <el-button @click="onClickSubmit">Submit</el-button>-->
-<!--        </div>-->
-<!--      </template>-->
+  <!--  <div class="mycard">-->
+  <!--    <el-card class="box-card">-->
+  <!--      <template #header>-->
+  <!--        <div class="card-header">-->
+  <!--          <el-icon color="white" class="no-inherit">-->
+  <!--            <Edit/>-->
+  <!--          </el-icon>-->
+  <!--          <span> Action</span>-->
+  <!--        </div>-->
+  <!--      </template>-->
+  <!--      <el-form label-width="80px" v-if="wallet_address">-->
+  <!--        <el-form-item label="Function:">-->
+  <!--          <el-select v-model="color" class="m-2" placeholder="Select" :disabled="lock">-->
+  <!--            <el-option-->
+  <!--                v-for="item in options"-->
+  <!--                :key="item.value"-->
+  <!--                :label="item.label"-->
+  <!--                :value="item.value"-->
+  <!--            >-->
+  <!--                        <span :style="{ color: item.value }">-->
+  <!--                          {{ item.label }}-->
+  <!--                        </span>-->
+  <!--            </el-option>-->
+  <!--          </el-select>-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Bitmap:">-->
+  <!--          <el-input :value="selected_map" disabled/>-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Owner:">-->
+  <!--          <el-tooltip-->
+  <!--              class="box-item"-->
+  <!--              effect="dark"-->
+  <!--              :content="wallet_address"-->
+  <!--              placement="top"-->
+  <!--          >-->
+  <!--            <el-input :value="shortend(wallet_address)" disabled/>-->
+  <!--          </el-tooltip>-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Soldier:">-->
+  <!--          <el-input-number :controls="false" :min="1" v-model="virus"/>-->
+  <!--        </el-form-item>-->
+  <!--      </el-form>-->
+  <!--      <template #footer>-->
+  <!--        <div style="display: flex; justify-content: right;" v-if="wallet_address">-->
+  <!--          <el-button @click="onClickSubmit">Submit</el-button>-->
+  <!--        </div>-->
+  <!--      </template>-->
 
-<!--    </el-card>-->
-<!--  </div>-->
+  <!--    </el-card>-->
+  <!--  </div>-->
 </template>
 
 <style scoped>
