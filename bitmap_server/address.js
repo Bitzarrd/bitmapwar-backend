ef12d54efe2b31551bd36a425867dd3933ede907
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-02-27 10:40:28 +0800

    rt

--- bitmap_server/address.js
+++ bitmap_server/address.js
@@ -7,14 +7,14 @@ export const pubKeyToEVMAddress = (pubKey) => {
     return address;
 };
 
-export const merlinAddress = () => {
-    var myHeaders = new Headers();
+export const evmAddressToMerlinAddress = (evmAddress) => {
+    let myHeaders = new Headers();
     myHeaders.append("authority", "rpc.particle.network");
     myHeaders.append("accept", "application/json, text/plain, */*");
     myHeaders.append("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7");
     myHeaders.append("content-type", "application/json");
-    myHeaders.append("origin", "http://localhost:3001");
-    myHeaders.append("referer", "http://localhost:3001/");
+    // myHeaders.append("origin", "http://localhost:3001");
+    // myHeaders.append("referer", "http://localhost:3001/");
     myHeaders.append("sec-ch-ua", "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"");
     myHeaders.append("sec-ch-ua-mobile", "?0");
     myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
@@ -23,32 +23,45 @@ export const merlinAddress = () => {
     myHeaders.append("sec-fetch-site", "cross-site");
     myHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
 
-    var raw = JSON.stringify({
+    let raw = JSON.stringify({
         "method": "particle_aa_getSmartAccount",
         "params": [
             {
                 "name": "BTC",
                 "version": "1.0.0",
-                "ownerAddress": "0x81FBF3bDA3fC90F01F35500Fb093C42eD2920bEC"
+                "ownerAddress": evmAddress
             }
         ],
-        "id": 1708962412974633,
+        // "id": 1708962412974633,
         "jsonrpc": "2.0"
     });
 
-    var requestOptions = {
+    let requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
 
-    fetch("https://rpc.particle.network/evm-chain?method=particle_aa_getSmartAccount&chainId=686868&projectUuid=a0c97b7b-ecd2-44c3-a8f8-545d5858f221&projectKey=c7npkknKRK6E44YKbBAhcsGRjHdXVC0hvMDmtGjK", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-
+    return new Promise((resolve, reject) => {
+        fetch("https://rpc.particle.network/evm-chain?method=particle_aa_getSmartAccount&chainId=686868&projectUuid=a0c97b7b-ecd2-44c3-a8f8-545d5858f221&projectKey=c7npkknKRK6E44YKbBAhcsGRjHdXVC0hvMDmtGjK", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                const data = JSON.parse(result);
+                resolve(data.result[0].smartAccountAddress);
+            })
+            .catch(error => {
+                console.log('error', error);
+                reject(error);
+            });
+    });
+    // fetch("https://rpc.particle.network/evm-chain?method=particle_aa_getSmartAccount&chainId=686868&projectUuid=a0c97b7b-ecd2-44c3-a8f8-545d5858f221&projectKey=c7npkknKRK6E44YKbBAhcsGRjHdXVC0hvMDmtGjK", requestOptions)
+    //     .then(response => response.text())
+    //     .then(result => console.log(result))
+    //     .catch(error => console.log('error', error));
+    //
+    //
+    // return "0x";
 }
 
 export const pubKeyToBtcAddress = (pubKey) => {
--- bitmap_server/test/test_address.js
+++ bitmap_server/test/test_address.js
@@ -1,13 +1,17 @@
-import {pubKeyToBtcAddress, pubKeyToEVMAddress} from "../address.js";
+import {evmAddressToMerlinAddress, pubKeyToBtcAddress, pubKeyToEVMAddress} from "../address.js";
 
-function test() {
+async function test() {
 
     const public_key = "0245e4eaf2ed3b3be91d65e4e668c279a9a081d7be8f8755c5269f7bf6766d4d27";
 
     const btc_address = pubKeyToBtcAddress(public_key);
     const evm_address = pubKeyToEVMAddress(public_key);
+    const merlin_address = await evmAddressToMerlinAddress(evm_address);
+
+    console.log("public_key", public_key);
     console.log("btc_address", btc_address);
     console.log("evm_address", evm_address)
+    console.log("merlin_address", merlin_address)
 }
 
 test();
