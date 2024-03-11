df965de6e98565c1aa3b87c232d339ddb9b804fb
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-03-11 10:57:21 +0800

    rt

--- bitmap_server/signature.js
+++ bitmap_server/signature.js
@@ -3,7 +3,7 @@ import {getBytes, id, solidityPackedKeccak256, verifyMessage, ethers} from "ethe
 export async function make_signature(privateKey, amount, nonce, to) {
     console.log("make_signature", privateKey, amount, nonce);
     const wallet = new ethers.Wallet(privateKey);
-    const messageHash = solidityPackedKeccak256(['uint256', 'uint256', 'address'], [(Number)(amount), nonce, to]);
+    const messageHash = solidityPackedKeccak256(['uint256', 'uint256', 'address'], [amount.toString(), nonce, to]);
     let messageHashBytes = getBytes(messageHash)
     return await wallet.signMessage(messageHashBytes);
 }
--- bitmap_server/test/test_makesignature.js
+++ bitmap_server/test/test_makesignature.js
@@ -6,7 +6,7 @@ dotenv.config();
 async function test() {
 
     const privateKey = "17294725c9cf12270b59508fcce1c33d7f405fb1ee0fa6d548fc478f25a855b3";//process.env.PRIVATE_KEY;
-    const amount = "1";
+    const amount = "60000000000000000";
     const nonce = 1;
     const to = "0x8E2B80470512F318C87fab1d2D3cEF45773AE571";
     const signature = await make_signature(privateKey, amount, nonce, to);
