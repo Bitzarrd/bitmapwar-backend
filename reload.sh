2fcd8c419c42b588f84b3276a5e2bffcf5d526d4
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-03-26 17:23:47 +0800

    e

---
+++ reload.sh
@@ -0,0 +1,14 @@
+if cd /www/wwwroot/bitmap3003 && git remote update && git status -uno | grep -q 'Your branch is behind'; then
+    echo "Git有更新可供拉取";
+    # 执行相应的操作
+    git pull;
+    echo "拉取完成";
+    mkdir -p "/www/wwwroot/bitmap_dev_client/packages/sample/public/static/$(git rev-parse --short HEAD)";
+    cp -rf /www/wwwroot/bitmap3003/Build/* "/www/wwwroot/bitmap_dev_client/packages/sample/public/static/$(git rev-parse --short HEAD)" ;
+    echo "window.gitRev= \"$(git rev-parse --short HEAD)\";" > "/www/wwwroot/bitmap_dev_client/packages/sample/public/static/rev.js";
+    echo "复制完成";
+    /www/server/panel/pyenv/bin/python /www/server/panel/pyenv/bin/supervisorctl -c /etc/supervisor/supervisord.conf restart dev_client:dev_client_00;
+    echo "重启完成";
+else
+    echo "Git没有更新";
+fi
