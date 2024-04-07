62675df42ee83ffaee5ff7f17f5458595e3c1d1c
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-04-07 16:15:57 +0800

    e

--- bitmap_server/rent.js
+++ bitmap_server/rent.js
@@ -23,6 +23,75 @@ export async function checkRent(bitmap_id) {
     }
 }
 
+// CREATE TABLE `rental` (
+//     `bitmap_id` int(11) NOT NULL,
+//     `days` int(11) NOT NULL DEFAULT '7',
+//     `timeout` int(11) NOT NULL DEFAULT '0',
+//     `owner` varchar(255) NOT NULL,
+//     `total_profit` varchar(255) NOT NULL DEFAULT '0',
+//     `total_btc` varchar(255) NOT NULL DEFAULT '0',
+//     `total_energy` varchar(255) NOT NULL DEFAULT '0',
+//     `type` varchar(255) NOT NULL DEFAULT 'energy',
+//     PRIMARY KEY (`bitmap_id`),
+//     KEY `rent_for_owner` (`owner`)
+// ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+export async function getRental(mysql_connection, bitmap_id) {
+    const sql = "SELECT * FROM rental WHERE bitmap_id = ?";
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            mysql_connection.query(sql, [bitmap_id], function (err, result, fields) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function insertRental(mysql_connection, bitmap_id, days, timeout, owner, total_profit, total_btc, total_energy, type) {
+    const sql = "INSERT INTO rental (bitmap_id, days, timeout, owner, total_profit, total_btc, total_energy, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+    try {
+        await new Promise((resolve, reject) => {
+            mysql_connection.query(sql, [bitmap_id, days, timeout, owner, total_profit, total_btc, total_energy, type], function (err, result, fields) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function updateRental(mysql_connection, bitmap_obj) {
+    const sql = "UPDATE rental SET days = ?, timeout = ?, owner = ?, total_profit = ?, total_btc = ?, total_energy = ?, type = ? WHERE bitmap_id = ?";
+
+    try {
+        await new Promise((resolve, reject) => {
+            mysql_connection.query(sql, [bitmap_obj.days, bitmap_obj.timeout, bitmap_obj.owner, bitmap_obj.total_profit, bitmap_obj.total_btc, bitmap_obj.total_energy, bitmap_obj.type, bitmap_obj.bitmap_id], function (err, result, fields) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    } catch (err) {
+        throw err;
+    }
+
+}
 
 async function test() {
     await checkRent(113111);
