472ffe6ba952c725ba94518657fbb43e57e1ba7b
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-04-26 13:35:04 +0800

    1

--- robot/readme.md
+++ robot/readme.md
@@ -1,3 +1,16 @@
 wss://dev-server.bitmapwar.com/api
 
-ts-node ./src/batch_join.ts -ws wss://dev-server.bitmapwar.com/api
+
+# Install
+sudo apt update
+sudo apt install nodejs
+sudo apt install ts-node
+sudo apt install node-typescript
+
+cd /mnt/d/robot
+npm install .
+
+# Run
+
+ts-node ./src/batch_join.ts -ws wss://dev-server.bitmapwar.com/api  -s 1 -a ./pubk.txt -c red
+
--- robot/src/batch_join.ts
+++ robot/src/batch_join.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import {program} from 'commander';
 import {WebSocket} from 'ws';
 import * as fs from "node:fs";
-import {RobotClient} from "./client";
 
 dotenv.config();
 
@@ -24,6 +23,7 @@ const logger = winston.createLogger({
 program
   .option('-a, --accounts <string>', 'accounts file path')
   .option('-s, --soldier <number>', 'soldier amount number')
+  .option('-c, --color <string>', 'color : red blue green purple')
   .option('-ws, --websocket_url <string>', 'websocket server');
 
 program.parse();
@@ -48,13 +48,23 @@ const main = async () => {
     return;
   }
 
+  if (typeof options.color === 'undefined') {
+    logger.error("color is required");
+    return;
+  }
+
+  if (typeof options.accounts === 'undefined') {
+    logger.error("accounts is required");
+    return;
+  }
+
 
   // logger.info("websocket url: " + options.websocket_url);
   const public_keys = load_public_key_array_from_path(options.accounts);
 
   for (let i = 0; i < public_keys.length; i++) {
     logger.info("public key: " + public_keys[i]);
-    new RobotClient(public_keys[i], options.websocket_url);
+    new RobotClient(public_keys[i], options.websocket_url,options.color, (Number)(options.soldier));
 
   }
 
@@ -64,6 +74,79 @@ const main = async () => {
 
 const sleep = async function (ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export class RobotClient {
+  public pk;
+  public color;
+  public virus;
+
+  constructor(pk: string, websocket_url: string, color: string, virus: number) {
+    this.pk = pk;
+    this.color = color;
+    this.virus = virus;
+    const myFormat = winston.format.printf(({level, message, timestamp}) => {
+      return `${timestamp} ${level} ${pk}: ${message}`;
+    });
+
+
+    const my_logger = winston.createLogger({
+      transports: [new winston.transports.Console()],
+      level: "debug",
+      defaultMeta: {service: 'user-service'},
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        myFormat
+      ),
+    });
+
+    const websocket_client = new WebSocket(websocket_url);
+
+    websocket_client.addListener('open', () => {
+      my_logger.info("connected to websocket server success");
+
+
+    });
+
+    websocket_client.on('message', function (event) {
+      // console.log();
+      // const json_str =  event.
+      // logger.info('Received message from server:' + event.toString());
+      let message = JSON.parse(event.toString())
+      {
+        if (message.method === 'Reload') {
+          const msg = JSON.stringify({
+            method: "Login",
+            address: pk
+          });
+          // console.log(msg);
+          this.send(msg);
+        } else if (message.method === 'LoginSuccess') {
+          my_logger.info(event.toString());
+          this.send(JSON.stringify({
+            method: "JoinGameBatch",
+            virus: virus,
+            color: color,
+          }));
+        } else if (message.method === 'JoinedGameBatchSuccess') {
+          my_logger.info(event.toString());
+        } else {
+          // my_logger.info('Received message from server:' + event.toString());
+        }
+      }
+    });
+
+// Connection closed
+    websocket_client.on('close', function (event) {
+      my_logger.info('Disconnected from the WebSocket server');
+    });
+
+// Error occurred
+    websocket_client.on('error', function (event) {
+      my_logger.error('WebSocket error:' + event);
+    });
+
+  }
 }
 
 main().then(r => {
