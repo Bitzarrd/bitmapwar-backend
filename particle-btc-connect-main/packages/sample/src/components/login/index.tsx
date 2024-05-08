cef34dd48d3352fdf37af74fe275f5682b3b9063
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-05-08 16:09:22 +0800

    1

--- particle-btc-connect-main/packages/sample/src/components/login/index.tsx
+++ particle-btc-connect-main/packages/sample/src/components/login/index.tsx
@@ -8,26 +8,26 @@ import {
   useConnector,
   useETHProvider,
 } from '@particle-network/btc-connectkit';
-import {useEffect, useState} from 'react';
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from '@nextui-org/react';
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from '@nextui-org/react';
-import {Input, Spinner} from '@nextui-org/react';
-import {Tooltip,} from '@nextui-org/tooltip';
+import { useEffect, useState } from 'react';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react';
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from '@nextui-org/react';
+import { Input, Spinner } from '@nextui-org/react';
+import { Tooltip } from '@nextui-org/tooltip';
 
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 // import {bgBlack} from "next/dist/lib/picocolors";
 // import { useLocation } from 'react-router-dom';
 import '../../styles/login.css';
 import BitMapWarAbi from './bitmapwar_abi.json';
-import {Contract, parseEther} from 'ethers';
+import { Contract, parseEther } from 'ethers';
 
 export default function Login() {
-  const {openConnectModal, disconnect} = useConnectModal();
-  const {accounts} = useAccounts();
-  const {evmAccount, chainId, switchChain, publicClient, getFeeQuotes, sendUserOp} = useETHProvider();
+  const { openConnectModal, disconnect } = useConnectModal();
+  const { accounts } = useAccounts();
+  const { evmAccount, chainId, switchChain, publicClient, getFeeQuotes, sendUserOp } = useETHProvider();
   // const location = useLocation();
 
-  const {provider, getNetwork, switchNetwork, signMessage, getPublicKey, sendBitcoin, sendInscription} =
+  const { provider, getNetwork, switchNetwork, signMessage, getPublicKey, sendBitcoin, sendInscription } =
     useBTCProvider();
   // const [gasless, setGasless] = useState<boolean>(false);
   // const [inscriptionReceiverAddress, setInscriptionReceiverAddress] = useState<string>();
@@ -35,9 +35,9 @@ export default function Login() {
   const [message, setMessage] = useState<string>('Login For Bitmapwar!');
   // const [address, setAddress] = useState<string>();
   // const [satoshis, setSatoshis] = useState<string>('1');
-  const {connectors, connect} = useConnector();
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const {isOpen: isOpenExtract, onOpen: onOpenExtract, onOpenChange: onOpenChangeExtract} = useDisclosure();
+  const { connectors, connect } = useConnector();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen: isOpenExtract, onOpen: onOpenExtract, onOpenChange: onOpenChangeExtract } = useDisclosure();
   const [forceHideModal, setForceHideModal] = useState<boolean>(false);
 
   const bitmapwarContractAddress = {
@@ -74,8 +74,8 @@ export default function Login() {
     console.log('tx', tx);
     const feeQuotes = await getFeeQuotes(tx);
     console.log('feeQuotes', feeQuotes);
-    const {userOp, userOpHash} = feeQuotes.verifyingPaymasterNative;
-    const hash = await sendUserOp({userOp, userOpHash}, forceHideModal);
+    const { userOp, userOpHash } = feeQuotes.verifyingPaymasterNative;
+    const hash = await sendUserOp({ userOp, userOpHash }, forceHideModal);
 
     toast.success('Transaction sent: ' + hash);
   };
@@ -193,7 +193,7 @@ export default function Login() {
     },
   ];
 
-  const columns_extract = [
+  const columnsExtract = [
     {
       key: 'id',
       label: 'ID',
@@ -205,6 +205,14 @@ export default function Login() {
     {
       key: 'amount',
       label: 'Amount',
+    },
+    {
+      key: 'create_time',
+      label: 'Create Time',
+    },
+    {
+      key: 'status',
+      label: 'Status',
     },
   ];
 
@@ -218,42 +226,42 @@ export default function Login() {
       {accounts.length === 0 && (
         <div className="btnBox">
           <div className="code-123456">CODE: {code}</div>
-          <br/> <br/>
+          <br /> <br />
           <Button color="warning" size="lg" onClick={openConnectModal} className="btn">
             Connect Wallet
           </Button>
         </div>
       )}
 
-      <br/>
+      <br />
       {accounts.length !== 0 && (
         <div className="btnBox">
           <div className="address">{accounts}</div>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Button color="primary" onClick={disconnect} size="lg" className="btn">
             Disconnect
           </Button>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Button color="primary" onClick={onGetPubkey} size="lg" className="btn">
             Enter Game
           </Button>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Button onPress={onOpen} size="lg" className="btn">
             Purchase Soldier
           </Button>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Button onPress={onOpenExtract} size="lg" className="btn">
             Extract Profit
           </Button>
         </div>
       )}
-      <br/>
+      <br />
 
-      <br/>
+      <br />
 
       <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="dark" size="5xl">
         <ModalContent>
@@ -263,7 +271,7 @@ export default function Login() {
               <ModalBody>
                 <Button color="warning" variant="flat">
                   Please do not close the browser or fresh the page during the recharge process.
-                  <br/>
+                  <br />
                   If you encounter any issues, please contact our customer service.
                 </Button>
                 <Tooltip content="1 Soilder = 0.03 BTC">
@@ -280,10 +288,10 @@ export default function Login() {
                   />
                 </Tooltip>
                 <Table aria-label="Example table with dynamic content" selectionMode="single">
-                  <TableHeader columns={columns}>
+                  <TableHeader columns={columnsExtract}>
                     {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
                   </TableHeader>
-                  <TableBody items={rows} isLoading={isLoading} loadingContent={<Spinner label="Loading..."/>}>
+                  <TableBody items={rows} isLoading={isLoading} loadingContent={<Spinner label="Loading..." />}>
                     {(item) => (
                       <TableRow key={item.key}>
                         {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
@@ -312,7 +320,7 @@ export default function Login() {
               <ModalBody>
                 <Button color="warning" variant="flat">
                   Please do not close the browser or fresh the page during the recharge process.
-                  <br/>
+                  <br />
                   If you encounter any issues, please contact our customer service.
                 </Button>
                 <Input
@@ -342,7 +350,7 @@ export default function Login() {
                   <TableHeader columns={columns}>
                     {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
                   </TableHeader>
-                  <TableBody items={rows} isLoading={isLoading} loadingContent={<Spinner label="Loading..."/>}>
+                  <TableBody items={rows} isLoading={isLoading} loadingContent={<Spinner label="Loading..." />}>
                     {(item) => (
                       <TableRow key={item.key}>
                         {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
